0ef09af2e25cfb8e7601b3498fa3e7826cf4e40e
Dani Rodríguez <danirod@outlook.com>
2016-04-07 18:09:21 +0200

    Add install script (Bourne Shell)
    In a single step this install script clones my vimrc repository,
    downloads vim-plug and executes the plug download script. It will
    be a one-liner copy and paste for quickly downloading my vimrc
    into a brand new machine.
    
    Quirks:
    * Doesn't check whether vim is installed or not.
    * Doesn't check whether git is installed or not.
    * Doesn't provide wget as an alternative when curl is not present.

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 # vimrc install script
 
-# Backup old setup
-[ -f ~/.vimrc ] && mv -f ~/.vimrc ~/.vimrc.bak
-[ -f ~/.gvimrc ] && mv -f ~/.gvimrc ~/.gvimrc.bak
+# Backup old setup (-f for overwriting old backups).
+[ -e ~/.vimrc ] && (rm -rf ~/.vimrc.bak; mv ~/.vimrc ~/.vimrc.bak)
+[ -e ~/.gvimrc ] && (rm -rf ~/.gvimrc.bak; mv ~/.gvimrc ~/.gvimrc.bak)
+[ -e ~/.vim ] && (rm -rf ~/.vim.bak; mv ~/.vim ~/.vim.bak)
 
-# Clone and install configuration
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# Clone and install configuration.
+git clone https://github.com/danirod/vimrc ~/.vim
+echo 'Downloading vim-plug...'
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Setting up symlinks.
+# Set up symbolic links.
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
 
-# Install configuration
+# Execute install.
 vim +PlugInstall +qall
 
