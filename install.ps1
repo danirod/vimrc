058d32f4e173a549fed1c3a3a8a26026b513daf2
Dani Rodríguez <danirod@outlook.com>
2016-04-12 09:22:39 +0200

    Add install script for Windows PowerShell
    Will backup ~\vimfiles to ~\vimfiles.bak (removes old backups)
    if it exists, then it will clone the vimrc repository into
    ~\vimfiles and install vim-plug.
    
    Again, this script is quirky because it assumes that Git and
    Vim are present in PATH. Should check that Git exists before
    attempting to start, and offer a workaround in case Vim is not
    found in PATH.

--- install.ps1
+++ install.ps1
@@ -1,9 +1,21 @@
-# vimrc installation script for Windows PowerShell
+# vimrc install script
 
-# Download vim-plug
-md ~\vimfiles\autoload
+# Backup old setup (-f overwrites old backups)
+if (Test-Path ~\vimfiles) {
+    if (Test-Path ~\vimfiles.bak) {
+        Remove-Item ~\vimfiles.bak -Recurse -Force
+    }
+    Move-Item ~\vimfiles -Destination ~\vimfiles.bak
+    $(Get-Item ~\vimfiles.bak -Force).Attributes = "Hidden"
+}
+
+# Clone and install configuration
+git clone https://github.com/danirod/vimrc $HOME\vimfiles
+$(Get-Item ~\vimfiles -Force).Attributes = "Hidden"
+echo 'Downloading vim-plug...'
+md ~\vimfiles\autoload -Force | Out-Null
 $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 (New-Object Net.WebClient).DownloadFile($uri, $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("~\vimfiles\autoload\plug.vim"))
 
-# Install plugins
+# Execute install
 vim +PlugInstall +qall
